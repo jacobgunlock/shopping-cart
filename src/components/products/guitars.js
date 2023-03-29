045cb4d3cc57f9fa96248c09af377a2d0cf5ef42
jacobgunlock <jacobkgundlach@gmail.com>
2023-03-29 14:03:10 -0500

    Inventory Setup

---
+++ src/components/products/guitars.js
@@ -0,0 +1,86 @@
+export const guitars = [
+  {
+    img: require('../../assets/guitars/acoustic/dark.jpg'),
+    name: 'Night Wings',
+    price: 289,
+    type: 'acoustic',
+  },
+  {
+    img: require('../../assets/guitars/acoustic/green.jpg'),
+    name: 'Green Jolly',
+    price: 199,
+    type: 'acoustic',
+  },
+  {
+    img: require('../../assets/guitars/acoustic/maple.jpg'),
+    name: 'Tree House Campfire',
+    price: 399,
+    type: 'acoustic',
+  },
+  {
+    img: require('../../assets/guitars/acoustic/oak.jpg'),
+    name: 'Hard Rock Wood',
+    price: 249,
+    type: 'acoustic',
+  },
+  {
+    img: require('../../assets/guitars/acoustic/sunkist.jpg'),
+    name: 'Sunkist Soda',
+    price: 449,
+    type: 'acoustic',
+  },
+  {
+    img: require('../../assets/guitars/acoustic/tan.jpg'),
+    name: 'Beach Body',
+    price: 289,
+    type: 'acoustic',
+  },
+  {
+    img: require('../../assets/guitars/electric/blue.jpg'),
+    name: 'Blue Devil',
+    price: 479,
+    type: 'electric',
+  },
+  {
+    img: require('../../assets/guitars/electric/cherry.jpg'),
+    name: 'Cherry Pie',
+    price: 349,
+    type: 'electric',
+  },
+  {
+    img: require('../../assets/guitars/electric/classic.jpg'),
+    name: `Toasted S'more`,
+    price: 469,
+    type: 'electric',
+  },
+  {
+    img: require('../../assets/guitars/electric/green.jpg'),
+    name: 'Magicka Potion',
+    price: 199,
+    type: 'electric',
+  },
+  {
+    img: require('../../assets/guitars/electric/mint.jpg'),
+    name: 'Shamrock Shake',
+    price: 599,
+    type: 'electric',
+  },
+  {
+    img: require('../../assets/guitars/electric/orange.jpg'),
+    name: 'Candy Crush',
+    price: 799,
+    type: 'electric',
+  },
+  {
+    img: require('../../assets/guitars/electric/red.jpg'),
+    name: 'Lipstick',
+    price: 629,
+    type: 'electric',
+  },
+  {
+    img: require('../../assets/guitars/electric/warhammer.jpg'),
+    name: 'The War Hammer',
+    price: 666,
+    type: 'electric',
+  },
+]
